91de85e928bfc7348782ad3d1ebb8433f65eea43
sajitheranda <118637181+sajitheranda@users.noreply.github.com>
2022-12-25 00:26:29 +0530

    I changed some parts in database conection

--- server/src/routes/employeeRoutes.js
+++ server/src/routes/employeeRoutes.js
@@ -1,1 +1,11 @@
-router.get("/:id", employees.findOne);
+module.exports = (app) => {
+    const employees = require("../controllers/employeeController");
+  
+    var router = require("express").Router();
+
+  
+    router.get("/:id", employees.findOne);
+  
+  
+    app.use("/employees", router);
+  };
