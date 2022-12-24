6c616ea8238bfb488cc546531f9a5c4f27611d37
asheneo <subodja162@gmail.com>
2022-12-24 23:56:04 +0530

    2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const authenticationRoutes = require('./src/routes/authenticationRoutes')
+//const authenticationRoutes = require('./src/routes/authenticationRoutes')
+require("./src/routes/employeeRoutes")(app);
 const app = express()
+app.use(express.json());
 
 require('./src/routes/authenticationRoutes')(app)
 
@@ -12,7 +14,9 @@ require('./src/routes/authenticationRoutes')(app)
 //     res.send("Hello");
 // })
 
-
+app.get("/", (req, res) => {
+    res.json({ message: "Successfully started" });
+  });
 
 
 app.listen(5000,()=>{console.log("Listening on port 5000")})
